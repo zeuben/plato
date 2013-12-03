de7036ed822274705f89f06d8da06e560f1b4e98
Jarrod Overson <jsoverson@gmail.com>
2013-12-03 09:59:21 -0800

    moved angular out of require

--- client-src/js/main.js
+++ client-src/js/main.js
@@ -4,10 +4,9 @@ requirejs.config({
 
 requirejs(
   [
-    'vendor/angular/angular',
     'app'
   ],
   function (app) {
-  //  angular.bootstrap(document, ['platoApp']);
+    angular.bootstrap(document, ['platoApp']);
   }
 );
